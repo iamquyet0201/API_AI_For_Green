4aac70209abab030a4dc82923ceb16ed2d5d67e0
Quyết <lehoangkhamedia28@gmail.com>
2025-08-05 00:17:28 +0700

    Update yolo_fastapi_server.py

--- yolo_fastapi_server.py
+++ yolo_fastapi_server.py
@@ -136,3 +136,6 @@ async def detection(file: UploadFile = File(...)):
         "msg": "success",
         "code": 200
     }
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
